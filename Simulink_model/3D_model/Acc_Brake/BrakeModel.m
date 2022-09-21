1aeb8991ddf388346ab036f78b53098009901f9d
mariotilocca98 <mario.tilocca@gmail.com>
2022-09-21 22:46:35 +0200

    Motor and Brake models implemented

---
+++ Simulink_model/3D_model/Acc_Brake/BrakeModel.m
@@ -0,0 +1,53 @@
+function [TwF,TwR] = BrakeModel(reqBrakeTorqueF, reqBrakeTorqueF, omegaF ,omegaR)
+
+    % ----------------------------------------------------------------
+    %% Function purpose: compute braking torques at front wheels with brake model
+    % ----------------------------------------------------------------
+
+    max_brake_torque = 476*2;
+    
+    regularSignScale = 1;  % find this value 
+    
+    % Check that the braking torques have correctly been specified as
+    % negative quantities. Otherwise, correct them by changing the sign
+    if (reqBrakeTorqueF > 0)
+        reqBrakeTorqueF = - reqBrakeTorqueF;
+    end
+
+    if (reqBrakeTorqueR > 0)
+        reqBrakeTorqueR = - reqBrakeTorqueR;
+    end
+    
+    
+    % Check that the requested braking torque is lower than the one that
+    % the hydraulic system can apply. 
+    % Use regularized sign functions (sin(atan(.))) to make sure that the 
+    % vehicle correctly stops at zero forward speed, to avoid that negative 
+    % speed values could be reached during braking
+
+
+    if (abs(reqBrakeTorqueF) < abs(max_brake_torque))
+
+        TwF = reqBrakeTorqueF*sin(atan(omegaF/regularSignScale)); % check rear wheels 
+
+    else
+
+        TwF = -max_brake_torque*sin(atan(omegaF/regularSignScale));
+
+    end
+
+
+    if (abs(reqBrakeTorqueR) < abs(max_brake_torque))
+
+        TwR = reqBrakeTorqueR*sin(atan(omegaR/regularSignScale));
+
+    else
+
+        TwR= -max_brake_torque*sin(atan(omegaR/regularSignScale));
+
+    end
+
+
+
+end
+
--- Simulink_model/3D_model/Tyres/wheelRotA.m
+++ Simulink_model/3D_model/Tyres/wheelRotA.m
@@ -1,24 +1,18 @@
-function w_dot = wheelRotA(Fx, f)
+function w_dot = wheelRotA(Fx, f, Tw)
+
+% implement the torque from motor 
+
 
 Iw = 80; % wheel inertia 
 
 % map the torque according to the vehicle velocity -- lookup table fashion 
 
+if f
+R= 1.87/(2*pi);
+else
+R=2.01 /(2*pi) ; 
+end
 
-R=0;
-hp = 250; 
-Tmax = 476;  % max corvette 1963 torque --- map torque later on 
-
-dFront = 3.5; 
-dRear = 2.5; 
-
-if f 
-    Tw = Tmax / dFront;    
-    R= 1.87/(2*pi);
-else 
-    Tw = Tmax/ dRear; 
-    R = 2.01/(2*pi);
-end 
 
 w_dot = (-Fx*R + Tw) / Iw; 
 
